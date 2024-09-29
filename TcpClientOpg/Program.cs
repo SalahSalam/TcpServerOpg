efe511d78a58a5599e6b7e38666e039171d4ef08
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2024-09-29 14:14:46 +0200

    Add project files.

---
+++ TcpClientOpg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+Console.WriteLine("TCP Client:");
+
+bool keepSending = true;
+
+TcpClient socket = new TcpClient("127.0.0.1", 7);
+
+NetworkStream ns = socket.GetStream();
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+
+while (keepSending)
+{
+    string message = Console.ReadLine();
+
+    writer.WriteLine(message);
+    writer.Flush();
+
+    string response = reader.ReadLine();
+
+    Console.WriteLine(response);
+
+    if (message.ToLower() == "close")
+    {
+        keepSending = false;
+        break;
+    }
+}
+
+socket.Close();
---
+++ TcpServerOpg/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("TCP Server");
+
+TcpListener listener = new TcpListener(IPAddress.Any, 7); // Port 7 for echo protocol or change to desired port.
+listener.Start();
+Console.WriteLine("Server started, waiting for connections...");
+
+while (true)
+{
+    TcpClient socket = listener.AcceptTcpClient();
+    IPEndPoint clientEndPoint = socket.Client.RemoteEndPoint as IPEndPoint;
+    Console.WriteLine("Client connected: " + clientEndPoint.Address);
+
+    Task.Run(() => HandleClient(socket));
+}
+
+listener.Stop();
+
+void HandleClient(TcpClient socket)
+{
+    NetworkStream ns = socket.GetStream();
+    StreamReader reader = new StreamReader(ns);
+    StreamWriter writer = new StreamWriter(ns) { AutoFlush = true };
+
+
+    while (socket.Connected)
+    {
+
+        string? message = reader.ReadLine();
+        if (message == null)
+        {
+            break;
+        }
+
+        Console.WriteLine("Received: " + message);
+
+        switch (message.ToLower())
+        {
+            case "random":
+                writer.WriteLine("input two numbers");
+                string numbers = reader.ReadLine();
+                string[] split = numbers.Split(" ");
+                if (split.Length == 2 && int.TryParse(split[0], out int a) && int.TryParse(split[1], out int b))
+                {
+                    Random random = new Random();
+                    writer.WriteLine(random.Next(a, b));
+                }
+                else
+                {
+                    writer.WriteLine("Invalid input. Please input two numbers.");
+                }
+
+                break;
+            case "add":
+                string numbers1 = reader.ReadLine();
+                string[] add = numbers1.Split();
+                if (add.Length == 2 && int.TryParse(add[0], out int x) && int.TryParse(add[1], out int y))
+                {
+
+                    writer.WriteLine(x + y);
+                }
+                else
+                {
+                    writer.WriteLine("Invalid input. Please input two numbers.");
+                }
+                break;
+            case "subtract":
+                string numbers2 = reader.ReadLine();
+                string[] sub = numbers2.Split(" ");
+                if (sub.Length == 2 && int.TryParse(sub[0], out int c) && int.TryParse(sub[1], out int d))
+                {
+
+                    writer.WriteLine(c - d);
+                }
+                else
+                {
+                    writer.WriteLine("Invalid input. Please input two numbers.");
+                }
+                break;
+            default:
+                writer.WriteLine("Unknown command");
+                break;
+        }
+
+    }
+}
